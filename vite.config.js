a6c1933d6392d061e5deaeb1a428b25d2c8f5c6c
Ricardo <donkbruno@hotmail.com>
2025-02-18 14:23:00 -0600

    Fix base path issue

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,30 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    "process.env.IS_PREACT": JSON.stringify("true"),
-  },
-  resolve: {
-    alias: {
-      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    define: {
+      "process.env.IS_PREACT": JSON.stringify("true"),
     },
-    server: {
-      port: 3001,
-      open: true,
-      hot: true
+    base: "/",
+    resolve: {
+      alias: {
+        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+      },
+      server: {
+        port: 3001,
+        open: true,
+        hot: true,
+      },
     },
+  };
+
+  if (command !== 'serve') {
+    config.base = "/FAQ_Category_Builder";
   }
-})
+
+  return config;
+});
